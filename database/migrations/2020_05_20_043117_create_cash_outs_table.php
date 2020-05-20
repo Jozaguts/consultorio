8d9fe5f3b7f39e10e60f3100f55baf72e74bf2c5
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-19 23:47:41 -0500

    add migration of cash_outs table

---
+++ database/migrations/2020_05_20_043117_create_cash_outs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCashOutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cash_outs', function (Blueprint $table) {
+            $table->id();
+            $table->bigIncrements('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->date('date');
+            $table->decimal('amount', 8, 2);
+            $table->date('cash_out_date');
+            $table->bigIncrements('consulting_room_id');
+            $table->foreign('consulting_room_id')->references('id')->on('consulting_rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cash_outs');
+    }
+}
