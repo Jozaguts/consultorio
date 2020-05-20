c15cc2b9e53e73a9ae3966b5acfe4ff8b3bd07bb
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-20 00:21:57 -0500

    add cash out model

--- app/CashOut.php
+++ app/CashOut.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class CashOut extends Model
 {
-    //
+    protected $table = 'cash_outs';
+
+    protected $fillable = ['user_id','date','amount','cash_out_date','consulting_room_id'];
+
+    
 }
