aa7edafa0f3315a9933af82763e877da0a2d4769
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-21 14:41:40 -0500

    add PaymentMethod Seeder

---
+++ database/seeds/PaymentMethodTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PaymentMethodTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\PaymentMethod::class, 10)->create();
+    }
+}
