b0aa2f2686528257e4b3261ef6d385c8754ec811
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-20 21:52:31 -0500

    add migration of patients table

---
+++ database/migrations/2020_05_21_015445_create_patients_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $$table->string('second_last_name');
+            $table->decimal('age',3,2);
+            $table->decimal('height',1,2);
+            $table->string('address');
+            $table->string('phone');
+            $table->string('contact');
+            $table->string('contact_phone');
+            $table->unsignedBigInteger('consulting_room_id');
+            $table->foreign('consulting_room_id')->references('id')->on('consulting_rooms');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
