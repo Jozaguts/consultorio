fc3f51b4b83d94a000e3da686d4716ff6efca0a2
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-21 20:31:59 -0500

    add Ãmigration payments table

---
+++ database/migrations/2020_05_22_012624_create_payments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('cash_out_id');
+            $table->foreign('cash_out_id')->references('id')->on('cash_outs');
+            $table->date('date');
+            $table->decimal('amount',8,2);
+            $table->unsignedBigInteger('payment_method_id');
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods');
+            $table->unsignedBigInteger('consulting_room_id');
+            $table->foreign('consulting_room_id')->references('id')->on('consulting_rooms');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
