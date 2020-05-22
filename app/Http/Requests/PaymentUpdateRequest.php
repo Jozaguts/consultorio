8b782846a056a513a1f50e268728666de3877aa4
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-21 21:04:04 -0500

    add validations of Payment model

---
+++ app/Http/Requests/PaymentUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cash_out_id' => 'exists:cash_outs',
+            'date' => 'string',
+            'amount' => 'min:1',
+            'payment_method_id' => 'exists:payment_methods',
+            'consulting_room_id' => 'exists:consulting_rooms',
+        ];
+    }
+}
